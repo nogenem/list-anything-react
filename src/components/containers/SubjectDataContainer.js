6463e200762ce74d89e49024e56b6f612ec496bf
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-12-03 17:36:32 -0200

    Adicionar sidemenu com tabs

---
+++ src/components/containers/SubjectDataContainer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Menu, Sidebar, Segment } from "semantic-ui-react";
+import { connect } from "react-redux";
+
+const SubjectDataContainer = ({ menuVisible, onMenuClick, children, tabs }) => (
+  <Sidebar.Pushable
+    className="subject-data-container"
+    as={Segment}
+    attached="bottom"
+    style={{ height: "86%" }}
+  >
+    <Sidebar
+      width="thin"
+      as={Menu}
+      animation="overlay"
+      icon="labeled"
+      visible={menuVisible}
+      vertical
+      inverted
+    >
+      {tabs.map(data => (
+        <Menu.Item key={data._id} tabid={data._id} onClick={onMenuClick}>
+          {data.description}
+        </Menu.Item>
+      ))}
+    </Sidebar>
+    <Sidebar.Pusher dimmed={menuVisible} style={{ overflowY: "auto" }}>
+      {children}
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
+);
+
+SubjectDataContainer.propTypes = {
+  menuVisible: PropTypes.bool.isRequired,
+  onMenuClick: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      description: PropTypes.string
+    })
+  ).isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    tabs: state.subject.tabs || []
+  };
+}
+
+export default connect(mapStateToProps)(SubjectDataContainer);
--- src/components/pages/SubjectPage.js
+++ src/components/pages/SubjectPage.js
@@ -4,74 +4,93 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import SubjectDataContainer from "../containers/SubjectDataContainer";
 import { fetchSubject, fetchSubjectData } from "../../actions/subjects";
 
 class SubjectPage extends Component {
   state = {
     loadingSubject: false,
-    loadingData: false
+    loadingData: false,
+    menuVisible: false
   };
 
-  componentDidMount = () => this.loadData(this.props);
+  componentDidMount = () => this.loadSubjects(this.props);
 
   componentWillReceiveProps(nextProps) {
     const currentId = this.props.match.params._id;
     const nextId = nextProps.match.params._id;
     if (currentId !== nextId) {
-      this.loadData(nextProps);
+      this.loadSubjects(nextProps);
     }
   }
 
-  loadData = props => {
-    this.setState({ loadingSubject: true });
-    props.fetchSubject(props.match.params._id).then(() => {
-      this.setState({ loadingSubject: false, loadingData: true });
-      props
-        .fetchSubjectData(null)
-        .then(() => this.setState({ loadingData: false }));
-    });
+  onMenuClick = (e, { tabid }) => {
+    this.setState({ menuVisible: false });
+    this.loadSubjectData(this.props, tabid);
   };
 
+  loadSubjects = props => {
+    this.setState({ loadingSubject: true });
+    props
+      .fetchSubject(props.match.params._id)
+      .then(() => this.loadSubjectData(props, null));
+  };
+
+  loadSubjectData = (props, _id) => {
+    this.setState({ loadingSubject: false, loadingData: true });
+    props
+      .fetchSubjectData(_id)
+      .then(() => this.setState({ loadingData: false }));
+  };
+
+  toggleMenu = () => this.setState({ menuVisible: !this.state.menuVisible });
+
   render() {
-    const { loadingData, loadingSubject } = this.state;
+    const { loadingData, loadingSubject, menuVisible } = this.state;
     const { subject } = this.props;
     const fields = subject.fields
       ? subject.fields.filter(field => field.show_in_list)
       : [];
     return (
       <Segment
-        style={{ maxWidth: "90%", margin: "10px auto" }}
+        style={{ maxWidth: "90%", margin: "10px auto", height: "92.3%" }}
         loading={loadingSubject}
       >
         <Header as="h2" color="teal" textAlign="center">
           {subject.description} Data
         </Header>
+        <Button icon="sidebar" onClick={this.toggleMenu} />
         <Button icon="add" positive primary as={Link} to="/subject-data/new" />
         {!loadingSubject && (
-          <Table celled compact>
-            <Table.Header>
-              <Table.Row>
-                {fields.map(field => (
-                  <Table.HeaderCell key={field._id}>
-                    {field.description}
-                  </Table.HeaderCell>
-                ))}
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {loadingData && (
+          <SubjectDataContainer
+            menuVisible={menuVisible}
+            onMenuClick={this.onMenuClick}
+          >
+            <Table celled compact>
+              <Table.Header>
                 <Table.Row>
-                  <Table.Cell width={16}>
-                    <Loader active inline="centered" />
-                  </Table.Cell>
+                  {fields.map(field => (
+                    <Table.HeaderCell key={field._id}>
+                      {field.description}
+                    </Table.HeaderCell>
+                  ))}
                 </Table.Row>
-              )}
+              </Table.Header>
+              <Table.Body>
+                {loadingData && (
+                  <Table.Row>
+                    <Table.Cell width={16}>
+                      <Loader active inline="centered" />
+                    </Table.Cell>
+                  </Table.Row>
+                )}
 
-              <Table.Row>
-                <Table.Cell>{/* Repensar isso... */}</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+                <Table.Row>
+                  <Table.Cell>{/* Repensar isso... */}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </SubjectDataContainer>
         )}
       </Segment>
     );

742ae849c40f34abdc7867479f28467a9b471dd8
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-12-13 14:32:07 -0200

    Terminar de adicionar o DateInputField

--- src/utils/renderFieldComponent.js
+++ src/utils/renderFieldComponent.js
@@ -7,6 +7,7 @@ import UrlInputImgField from "../components/fields/UrlInputImgField";
 import NumberInputField from "../components/fields/NumberInputField";
 import TextareaField from "../components/fields/TextareaField";
 import UrlInputField from "../components/fields/UrlInputField";
+import DateInputField from "../components/fields/DateInputField";
 
 export default fieldData => {
   const { field } = fieldData;
@@ -21,6 +22,8 @@ export default fieldData => {
       return <TextareaField {...fieldData} />;
     case fieldTypes.URL_INPUT:
       return <UrlInputField {...fieldData} />;
+    case fieldTypes.DATE_INPUT:
+      return <DateInputField {...fieldData} />;
     default:
       return <div key={field._id} />;
   }

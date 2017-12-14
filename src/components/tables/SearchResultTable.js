66b5c93e232b65af2987ab68efd8774c05dfe8ef
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-12-14 13:09:18 -0200

    Adicionar opção de ordenar as tabelas de dados

--- src/components/tables/SearchResultTable.js
+++ src/components/tables/SearchResultTable.js
@@ -1,26 +1,77 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Table } from "semantic-ui-react";
+import sortBy from "lodash.sortby";
 
 class SearchResultTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      column: null,
+      data: props.results,
+      direction: null
+    };
+  }
+
+  componentWillReceiveProps = nextProps => {
+    if (this.props.results !== nextProps.results) {
+      this.setState({ data: nextProps.results });
+    }
+  };
+
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state;
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: sortBy(data, [clickedColumn]),
+        direction: "ascending"
+      });
+    } else {
+      this.setState({
+        data: data.reverse(),
+        direction: direction === "ascending" ? "descending" : "ascending"
+      });
+    }
+  };
+
   render() {
-    const { results, onTableRowClick } = this.props;
+    const { onTableRowClick } = this.props;
+    const { column, data, direction } = this.state;
+
     return (
-      <Table celled compact="very" selectable>
+      <Table celled compact="very" selectable sortable>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Value</Table.HeaderCell>
-            <Table.HeaderCell>Tab</Table.HeaderCell>
-            <Table.HeaderCell>Subject</Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={column === "value" ? direction : null}
+              onClick={this.handleSort("value")}
+            >
+              Value
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={column === "tab" ? direction : null}
+              onClick={this.handleSort("tab")}
+            >
+              Tab
+            </Table.HeaderCell>
+            <Table.HeaderCell
+              sorted={column === "subject" ? direction : null}
+              onClick={this.handleSort("subject")}
+            >
+              Subject
+            </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body style={{ cursor: "pointer" }}>
-          {!results.length && (
+          {!data.length && (
             <Table.Row>
               <Table.Cell width={16}>Nothing was found.</Table.Cell>
             </Table.Row>
           )}
-          {results.map(result => (
+          {data.map(result => (
             <Table.Row
               key={result._id}
               to={`/subject-data/${result._id}`}
--- src/components/tables/SubjectDataTable.js
+++ src/components/tables/SubjectDataTable.js
@@ -1,72 +1,118 @@
 import React from "react";
 import { Table, Loader } from "semantic-ui-react";
 import PropTypes from "prop-types";
+import sortBy from "lodash.sortby";
 
 import renderFieldComponent from "../../utils/renderFieldComponent";
 
-const renderField = (data, field) => {
-  const fieldData = {
-    key: data._id,
-    value: data.value,
-    showFieldDescription: false,
-    field
+class SubjectDataTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      column: null,
+      data: props.subjectDataArray,
+      direction: null
+    };
+  }
+
+  componentWillReceiveProps = nextProps => {
+    if (this.props.subjectDataArray !== nextProps.subjectDataArray) {
+      this.setState({ data: nextProps.subjectDataArray });
+    }
   };
-  return renderFieldComponent(fieldData);
-};
 
-// Talvez seja necessário lidar com outros tipos também...
-const getCellWidth = fieldType =>
-  fieldType === "url_input_img" ? { width: 2 } : {};
+  // Talvez seja necessário lidar com outros tipos também...
+  getCellWidth = fieldType =>
+    fieldType === "url_input_img" ? { width: 2 } : {};
 
-const renderCell = (subjectData, field) => {
-  const data = subjectData.data[field._id];
-  return (
-    <Table.Cell {...getCellWidth(field.field_type)} collapsing key={data._id}>
-      {renderField(data, field)}
-    </Table.Cell>
-  );
-};
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state;
 
-const SubjectDataTable = ({
-  loading,
-  onTableRowClick,
-  fields,
-  subjectDataArray
-}) => (
-  <Table celled compact="very" selectable>
-    <Table.Header>
-      <Table.Row>
-        {fields.map(
-          field =>
-            field.show_in_list && (
-              <Table.HeaderCell key={field._id}>
-                {field.description}
-              </Table.HeaderCell>
-            )
-        )}
-      </Table.Row>
-    </Table.Header>
-    <Table.Body style={{ cursor: "pointer" }}>
-      {loading && (
-        <Table.Row>
-          <Table.Cell width={16}>
-            <Loader active inline="centered" />
-          </Table.Cell>
-        </Table.Row>
-      )}
-      {!loading &&
-        subjectDataArray.map(sd => (
-          <Table.Row
-            key={sd._id}
-            to={`/subject-data/${sd._id}`}
-            onClick={onTableRowClick}
-          >
-            {fields.map(field => field.show_in_list && renderCell(sd, field))}
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: sortBy(data, [o => o.data[clickedColumn].value]),
+        direction: "ascending"
+      });
+    } else {
+      this.setState({
+        data: data.reverse(),
+        direction: direction === "ascending" ? "descending" : "ascending"
+      });
+    }
+  };
+
+  renderCell = (subjectData, field) => {
+    const data = subjectData.data[field._id];
+    return (
+      <Table.Cell
+        {...this.getCellWidth(field.field_type)}
+        collapsing
+        key={data._id}
+      >
+        {this.renderField(data, field)}
+      </Table.Cell>
+    );
+  };
+
+  renderField = (data, field) => {
+    const fieldData = {
+      key: data._id,
+      value: data.value,
+      showFieldDescription: false,
+      field
+    };
+    return renderFieldComponent(fieldData);
+  };
+
+  render() {
+    const { loading, onTableRowClick, fields } = this.props;
+    const { column, direction, data } = this.state;
+
+    return (
+      <Table celled compact="very" selectable sortable>
+        <Table.Header>
+          <Table.Row>
+            {fields.map(
+              field =>
+                field.show_in_list && (
+                  <Table.HeaderCell
+                    key={field._id}
+                    sorted={column === field._id ? direction : null}
+                    onClick={this.handleSort(field._id)}
+                  >
+                    {field.description}
+                  </Table.HeaderCell>
+                )
+            )}
           </Table.Row>
-        ))}
-    </Table.Body>
-  </Table>
-);
+        </Table.Header>
+        <Table.Body style={{ cursor: "pointer" }}>
+          {loading && (
+            <Table.Row>
+              <Table.Cell width={16}>
+                <Loader active inline="centered" />
+              </Table.Cell>
+            </Table.Row>
+          )}
+          {!loading &&
+            data.map(sd => (
+              <Table.Row
+                key={sd._id}
+                to={`/subject-data/${sd._id}`}
+                onClick={onTableRowClick}
+              >
+                {fields.map(
+                  field => field.show_in_list && this.renderCell(sd, field)
+                )}
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
+    );
+  }
+}
 
 SubjectDataTable.propTypes = {
   // ownProps

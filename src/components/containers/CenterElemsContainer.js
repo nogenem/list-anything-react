431fc1df14077baa5f8669fe0680f922e10968ff
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-12-14 13:42:51 -0200

    Remover div desnecessária

--- src/components/containers/CenterElemsContainer.js
+++ src/components/containers/CenterElemsContainer.js
@@ -3,11 +3,9 @@ import PropTypes from "prop-types";
 import { Grid } from "semantic-ui-react";
 
 const CenterElemsContainer = ({ children }) => (
-  <div id="center-elems-container">
-    <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>{children}</Grid.Column>
-    </Grid>
-  </div>
+  <Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>{children}</Grid.Column>
+  </Grid>
 );
 
 CenterElemsContainer.propTypes = {

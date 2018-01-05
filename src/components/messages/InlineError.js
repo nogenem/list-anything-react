c5e1a7d8ebb6231af6d4061ef4864b3c2142eb3a
gilney nathanael mathias <gilney_salvo@hotmail.com>
2018-01-05 19:17:45 -0200

    Mudar o span para div

--- src/components/messages/InlineError.js
+++ src/components/messages/InlineError.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const InlineError = ({ text }) => (
-  <span style={{ color: "#ae5856" }}>{text}</span>
-);
+const style = {
+  color: "#ae5856",
+  textAlign: "left",
+  minWidth: "100%"
+};
+
+const InlineError = ({ text }) => <div style={style}>{text}</div>;
 
 InlineError.propTypes = {
   // ownProps
